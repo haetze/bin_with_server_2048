f0989030002f33d7cc88dffbcc56cd49613c3db6
Richard Stewing <richy.sting@gmail.com>
2018-07-29 23:40:09 +0200

    simple printing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
+#![allow(unused_must_use)]
+
+extern crate ncurses;
+
+use ncurses::*;
+
 use std::net::TcpStream;
 use std::net::SocketAddr;
 use std::io::prelude::*;
 use std::env;
 
+
 const DEFAULT_PORT: u16 = 4343;
 const DEFAULT_SIZE: u8  = 4;
 
 fn main() {
+    initscr();
+    
     let port_requested: u16 = match env::args().skip(2).next() {
         Some(p) => match p.parse() {
             Ok(port) => port,
@@ -34,12 +43,40 @@ fn main() {
 }
 
 
-fn start_game(mut socket: TcpStream, size: u8) {
+fn start_game(socket: TcpStream, size: u8) {
     use std::io::BufReader;
     
     let mut socket = BufReader::new(socket);
-    let create_command = format!("new {}", size);
-
+    let create_command = format!("new {}\n", size);
     socket.get_mut().write(create_command.as_bytes());
+    noecho();
+    loop {
+        clear();
+        let mut res = String::new();
+        socket.read_line(&mut res);
+        print_response(res);
+        let _ = match getch() as u8 as char {
+            'r' => socket.get_mut().write(b"right\n"),
+            'l' => socket.get_mut().write(b"left\n"),
+            'u' => socket.get_mut().write(b"up\n"),
+            'd' => socket.get_mut().write(b"down\n"),
+            'q' => {
+                socket.get_mut().write(b"exit\n");
+                break;
+            },
+            _  => Ok(0),
+            
+        };
+        printw("======================\n");
+        
+    }
     
+}
+
+fn print_response(response: String) {
+    let lines = response.split(';');
+    for l in lines {
+        printw(l);
+        printw("\n");
+    }
 }
