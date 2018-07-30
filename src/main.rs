758b40b9ec559910b7c8f9a8af81676e511deb2a
Richard Stewing <richy.sting@gmail.com>
2018-07-30 22:15:26 +0200

    allow wrong command input

--- src/main.rs
+++ src/main.rs
@@ -56,25 +56,38 @@ fn start_game(socket: TcpStream, size: u8) {
         let mut res = String::new();
         socket.read_line(&mut res);
         print_response(res);
-        let _ = match getch() as u8 as char {
-            'r' => socket.get_mut().write(b"right\n"),
-            'l' => socket.get_mut().write(b"left\n"),
-            'u' => socket.get_mut().write(b"up\n"),
-            'd' => socket.get_mut().write(b"down\n"),
-            'q' => {
-                socket.get_mut().write(b"exit\n");
-                break;
-            },
-            _  => Ok(0),
-            
-        };
-        printw("======================\n");
+        let mut break_out = false;
+        loop {
+            let c = getch() as u8 as char;
+            let result = match c {
+                'r' => socket.get_mut().write(b"right\n"),
+                'l' => socket.get_mut().write(b"left\n"),
+                'u' => socket.get_mut().write(b"up\n"),
+                'd' => socket.get_mut().write(b"down\n"),
+                'q' => {
+                    break_out = true;
+                    socket.get_mut().write(b"exit\n")
+                },
+                _  => Ok(0),
+                
+            };
+            match result {
+                Ok(0) => continue,
+                Ok(_) => break,
+                _     => {},
+            }
+         
+        }
+        if break_out {
+            break;
+        }
         
     }
     
 }
 
 fn print_response(response: String) {
+    printw(response.as_str());
     let lines = response.split(';');
     for l in lines {
         if l == "\n" {
